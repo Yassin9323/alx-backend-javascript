9c95d8127b75a6bd83b0bbbc6c9c5954a09ea292
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-16 15:43:20 +0300

    task 11

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function createEmployeesObject(departmentName, employees) {
+  const employeeObject = {
+    [departmentName]: [...employees],
+  };
+  return employeeObject;
+}
