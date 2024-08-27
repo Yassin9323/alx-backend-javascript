f070fac2b4cb7b8adb8024486522291e7ec05fc8
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 02:50:13 +0300

    task 5

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+export default function uploadPhoto(filename) {
+  return new Promise((resolve, reject) => {
+    const value = false;
+    if (value) {
+      resolve(filename);
+    } else {
+      reject(Error(`${filename} cannot be processed`));
+    }
+  });
+}
