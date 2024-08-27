74d64439d0d9c82f394e6dacbf4b5b18ca4b1c54
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 02:09:02 +0300

    task 2

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    // const value = success;
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
