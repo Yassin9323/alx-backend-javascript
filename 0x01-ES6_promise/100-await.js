75e106df744c1cb918df5257c282ace051e39ee0
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 03:31:18 +0300

    advanced

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(
+      ([photo, user]) => ({
+        photo,
+        user,
+      }),
+    )
+    .catch(() => ({
+      photo: null,
+      user: null,
+    }));
+}
