52351877a9cf2b030a98b85cf8671846235a2e6a
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 03:16:54 +0300

    task7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload])
+    .then((result) => result)
+    .catch((error) => {
+      console.error(error);
+    });
+}
