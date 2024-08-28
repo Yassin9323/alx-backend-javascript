e297fcda069a556411c9efea6d241e7c2a54912c
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 03:22:14 +0300

    task 8, 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const math = mathFunction();
+    queue.push(math);
+  } catch (error) {
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
