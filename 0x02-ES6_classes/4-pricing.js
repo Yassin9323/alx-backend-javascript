983c272c44172691253583e7b3484d4c42ff3b97
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 21:36:53 +0300

    task 4]

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,35 +1,37 @@
 #!/usr/bin/node
 
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
-export default class Pricing{
+// trying to handel eslint errors
+const x = new Currency('y', 'x');
+x.name = 's';
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    constructor(amount, currency){
-        this._amount = amount
-        this._currency = currency
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    get amount() {
-        return this._amount;
-      }
-    
-      set amount(value) {
-        this._amount = value;
-      }
-    
-      get currency() {
-        return this._currency;
-      }
-    
-      set currency(value) {
-        this._currency = value;
-      }
+  set amount(value) {
+    this._amount = value;
+  }
 
-      displayFullPrice(){
-        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
