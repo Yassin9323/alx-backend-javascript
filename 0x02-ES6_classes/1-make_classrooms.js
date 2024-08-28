31b89c5277d2095289ba777a76a1d1b145f50992
Yassin9323 <yassin.waleed94@gmail.com>
2024-08-28 20:50:12 +0300

    task 2

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,16 +2,16 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const arr = [];
+  // const arr = [];
 
-  const x = new ClassRoom(10);
-  const y = new ClassRoom(10);
-  const z = new ClassRoom(10);
+  // const x = new ClassRoom(10);
+  // const y = new ClassRoom(10);
+  // const z = new ClassRoom(10);
 
-  arr.push(x, y, z);
+  // arr.push(x, y, z);
 
-  return arr;
+  // return arr;
 
-//   another solution
-//   return [19, 20, 34].map((size) => new ClassRoom(size));
+  // another solution
+  return [19, 20, 34].map((size) => new ClassRoom(size));
 }
