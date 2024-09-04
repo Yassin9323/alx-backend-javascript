0ef0cf9150a893ac89ead4f837a24a15fe2b6b72
Yassin9323 <yassin.waleed94@gmail.com>
2024-09-04 20:21:06 +0300

    up to task 10

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+export default function cleanSet(set, startString) {
+  // If startString is not provided or empty, return an empty string
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const filteredArray = [...set].filter((value) => value.startsWith(startString));
+
+  const resultArray = filteredArray.map((value) => value.slice(startString.length));
+
+  // Join the resulting values into a single string separated by hyphens
+  return resultArray.join('-');
+}
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+export default function groceriesList() {
+  // Create a new Map and add the grocery items with their quantities
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+
+  return groceries;
+}
